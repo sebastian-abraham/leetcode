47be7e58440da98f6dcff233948589e6e6a018a7
Sebastian Abraham <sebastianabraham2006@gmail.com>
2026-07-03 08:36:28 +0000

    Sync LeetCode submission Runtime - 7 ms (91.50%), Memory - 19.4 MB (45.16%)

--- solutions/0242-valid-anagram/solution.py
+++ solutions/0242-valid-anagram/solution.py
@@ -1,9 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        tarr = [*t]
+        if len(s) != len(t):
+            return False
+        
+        hmaps = [0]*26
+        hmapt = [0]*26
+
         for c in s:
-            if c in tarr:
-                tarr.remove(c)
-            else:
-                return False
-        return len(tarr) == 0
+            hmaps[ord(c) - ord('a')] += 1
+
+        for c in t:
+            hmapt[ord(c) - ord('a')] += 1
+
+        return(hmaps == hmapt)
+
+
